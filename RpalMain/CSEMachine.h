5f091942ac0cb82fb004c931d6287870ee775fe7
itskaizad <kaizad.avari@live.com>
2016-04-22 01:43:30 -0400

    Working flattener.
    Only CSE Machine remains. Flattener with fancy printing complete.

--- RpalMain/CSEMachine.h
+++ RpalMain/CSEMachine.h
@@ -35,40 +35,54 @@ public:
 	{
 		if (mTree == NULL)
 			return;
+
+		//Helper to debug how deltas are made for the control structures.
+		//cout << index << ".\t" << mTree->nodeValue << " is a " << mTree->nodeType << endl;
+
 		// Make sure controls[index] exists.
 		// If not, youre entering that delta first time, so create it!
 		if (controls.size() < index)
 		{
 			//TODO Check this out! Hopefully * gives the new object from the pointer.
 			controls.push_back(*(new vector<CSElement>));
+			//PSST: It does!
 		}
 
 		CSElement* currentEl;
 		if (mTree->nodeValue == "lambda")
 		{
 			string varName = mTree->childNode->nodeValue;
-			currentEl = new CSElement("delta", index + 1, varName, "");
+			int deltaNumber = controls.size();
+			currentEl = new CSElement("lambda", deltaNumber, varName, "");
 			controls[index - 1].push_back(*currentEl);
-			buildControlStructures(mTree->childNode->siblingNode, index + 1);
+			//if (mTree->childNode != NULL)		Commented him out assuming theres always a right child to LAMBDA.
+			buildControlStructures(mTree->childNode->siblingNode, deltaNumber + 1);
 		}
 		else
 		{
 			currentEl = new CSElement(mTree->nodeValue);
 			controls[index - 1].push_back(*currentEl);
 			buildControlStructures(mTree->childNode, index);
-			buildControlStructures(mTree->siblingNode, index);
+			if(mTree->childNode != NULL)
+				buildControlStructures(mTree->childNode->siblingNode, index);
 		}
 
 		
 	}
 
+
+	// To print out every control structure (delta) we have in Vector 'controls'
 	void printControlStructures()
 	{
 		for (int i = 0; i < controls.size(); i++)
 		{
+			cout << i << ".\t";
 			for (int j = 0; j < controls[i].size(); j++)
 			{
-				cout << controls[i].at(j).value << " ";
+				if (controls[i].at(j).value == "lambda")
+					cout << controls[i].at(j).value << controls[i].at(j).index << " ";
+				else
+					cout << controls[i].at(j).value << " ";
 			}
 			cout << endl;
 		}
